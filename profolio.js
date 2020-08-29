678237ec977bffca1c078095b3b4a5cc1ae6f39b
Duc <vuminhduc796@gmail.com>
2020-08-29 13:20:39 +1000

    Update 29/8

--- profolio.js
+++ profolio.js
@@ -2,19 +2,24 @@ function switchConcept() {
     document.getElementById("tech01").innerHTML = "OOP Design";
     document.getElementById("tech02").innerHTML = "Data Structure and Algorithm";
     document.getElementById("tech03").innerHTML = "Web Development";
+    document.getElementById("tech04").innerHTML = "Android App Development"
+    
     document.getElementById("button-language").style.backgroundColor = "#cfcfcf";
     document.getElementById("button-concept").style.backgroundColor = "#e3e3e3";
     document.getElementById("tech11").style.width = "60%";
     document.getElementById("tech12").style.width = "70%";
     document.getElementById("tech13").style.width = "45%";
+    document.getElementById("tech14").style.width = "55%";
 }
 function switchLanguage() {
     document.getElementById("tech01").innerHTML = "Java";
     document.getElementById("tech02").innerHTML = "Python";
     document.getElementById("tech03").innerHTML = "HTML + JS + CSS";
+    document.getElementById("tech04").innerHTML = "C and Assembly Languanges"
     document.getElementById("button-concept").style.backgroundColor = "#cfcfcf";
     document.getElementById("button-language").style.backgroundColor = "#e3e3e3";
     document.getElementById("tech11").style.width = "70%";
     document.getElementById("tech12").style.width = "80%";
     document.getElementById("tech13").style.width = "55%";
+    document.getElementById("tech14").style.width = "42%";
 }
